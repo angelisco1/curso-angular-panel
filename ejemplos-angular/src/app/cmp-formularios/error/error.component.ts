b52c0092db360318e768de37102b6b81ff6924d7
AngelVil92 <angelvil92@gmail.com>
2021-03-18 20:03:25 +0100

    Día 4: formularios

--- ejemplos-angular/src/app/cmp-formularios/error/error.component.ts
+++ ejemplos-angular/src/app/cmp-formularios/error/error.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.css']
 })
-export class ErrorComponent implements OnInit {
+export class ErrorComponent implements OnInit, OnChanges {
+
+  @Input() error;
+  errorKey: string;
 
   constructor() { }
 
+  ngOnChanges(changes) {
+    // console.log(changes);
+    if (!changes.error.previousValue || (!changes.error.firstChange && Object.keys(changes.error.currentValue)[0] !==  Object.keys(changes.error.previousValue)[0])) {
+      console.log('Cambia el errorKey', changes)
+      this.errorKey = Object.keys(this.error)[0];
+    }
+  }
+
   ngOnInit(): void {
+  //   console.log(this.error);
+  //   this.errorKey = Object.keys(this.error)[0];
   }
 
 }
--- ejemplos-angular/src/app/cmp-formularios/reactivos/reactivos.component.ts
+++ ejemplos-angular/src/app/cmp-formularios/reactivos/reactivos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactivos',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactivos.component.css']
 })
 export class ReactivosComponent implements OnInit {
+  formulario: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.formulario = new FormGroup({
+      usuario: new FormControl('angel', [Validators.required, this.esStark]),
+      password: new FormControl('1234', [Validators.required, Validators.minLength(8)])
+    });
   }
 
+  login() {
+    console.log(this.formulario)
+  }
+
+  esStark(formControl) {
+    const starks = ['arya', 'robb', 'sansa', 'rickon', 'tony', 'bran'];
+    if (starks.includes(formControl.value.toLowerCase())) {
+      return null;
+    }
+    return {
+      esStark: {
+        actualName: formControl.value,
+        validNames: starks
+      }
+    }
+  }
 }
